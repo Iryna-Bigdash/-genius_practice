9b55e050a15cad53671e312db5c801d3899cc46b
Iryna B <imbigdash@gmail.com>
2023-04-07 00:02:39 +0100

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const titleEl = document.querySelector('h1');
+titleEl.addEventListener('mouseover', onHover);
+
+function onHover() {
+titleEl.innerHTML = 'Helloy! It is my first code on Java Script whith Genius';
+}
