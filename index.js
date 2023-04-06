62d4269ff04989f29952e4bd78a8945fa11f9d07
Iryna B <imbigdash@gmail.com>
2023-04-07 00:12:53 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const titleEl = document.querySelector('h1');
-titleEl.addEventListener('mouseover', onHover);
+titleEl.addEventListener('mouseover', onHover, false);
+titleEl.addEventListener('mouseout', outHover, false);
 
 function onHover() {
-titleEl.innerHTML = 'Helloy! It is my first code on Java Script whith Genius';
+    titleEl.innerHTML = 'Helloy! It is my first code on Java Script whith Genius';
+    titleEl.setAttribute('style', "color:blue;");
+}
+
+function outHover() {
+    titleEl.innerHTML = "It is my first lesson"; 
+    titleEl.setAttribute('style', 'color:red;');
 }
