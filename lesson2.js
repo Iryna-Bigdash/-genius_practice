66e59657b39687395d68f04d26b8af179a2a0ac5
Iryna B <imbigdash@gmail.com>
2023-04-10 13:24:10 +0100

    added hw-2

---
+++ lesson2.js
@@ -0,0 +1,81 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// let number = 1;
+
+// number > 0 ? console.log("true") : console.log("false");
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// let value = 'test';
+// value === 'test'? console.log(true) : console.log(false) ;
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// let value = 13;
+// if (value >= 10) {
+//     console.log(value - 5);
+// } else {
+//    console.log(value + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+// let mounth = 41;
+
+// switch (mounth) {
+//   case 1:
+//     console.log('January');
+//     break;
+//     case 2:
+//     console.log('Fabruary');
+//     break;
+//   case 3:
+//     console.log('March');
+//     break;
+//   case 4:
+//     console.log('April');
+//     break;
+//   case 5:
+//     console.log('May');
+//     break;
+//   case 6:
+//     console.log('June');
+//     break;
+//   case 7:
+//     console.log('July');
+//     break;
+//   case 8:
+//     console.log('Augest');
+//     break;
+//   case 9:
+//     console.log('September');
+//     break;
+//   case 10:
+//     console.log('October');
+//     break;
+//   case 11:
+//     console.log('November');
+//     break;
+//   case 12:
+//     console.log('December');
+//     break;
+
+//   default:
+//     console.log('Enter a number from 1 to 12');
+//     break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let massage = prompt('тризначне число').trim();
+// console.log(massage);
+ 
+// let sum = massage.split('').reduce((acc, val) => 
+//     acc + parseInt(val), 0);
+// alert(sum);
+
